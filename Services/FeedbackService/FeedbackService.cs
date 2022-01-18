4164afce9b695c3d76c72db35e6da0e252c0276a
AngelVasilev <defiend@abv.bg>
2022-01-18 22:53:50 +0200

    Admin can change his password.

--- Services/FeedbackService/FeedbackService.cs
+++ Services/FeedbackService/FeedbackService.cs
@@ -1,5 +1,6 @@
 ﻿namespace Services.FeedbackService
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
@@ -32,7 +33,8 @@
                 FirstName = model.FirstName,
                 LastName = model.LastName,
                 Email = model.Email,
-                Type = model.Type
+                Type = model.Type,
+                Comment = model.Comment
             };
 
             await this.data.Feedbacks.AddAsync(feedback);
@@ -42,7 +44,51 @@
         public async Task<ICollection<FeedbackViewModel>> GetUserFeedbacks(string email) 
             => await this.data.Feedbacks
                 .Where(x => x.Email == email && x.IsDeleted == false)
+                .OrderByDescending(x => x.CreatedOn)
                 .ProjectTo<FeedbackViewModel>(this.mapper.ConfigurationProvider)
                 .ToListAsync();
+
+        public async Task<ICollection<FeedbackViewModel>> GetAll()
+             => await this.data.Feedbacks
+                    .Where(x => x.IsDeleted == false)
+                    .OrderByDescending(x => x.CreatedOn)
+                    .ProjectTo<FeedbackViewModel>(this.mapper.ConfigurationProvider)
+                    .ToListAsync();
+
+        public async Task<bool> Delete(int id)
+        {
+            var model = await this.data.Feedbacks
+                    .Where(x => x.Id == id && x.IsDeleted == false)
+                    .FirstOrDefaultAsync();
+
+            if (model == null)
+            {
+                return false;
+            }
+
+            model.IsDeleted = true;
+            model.DeletedOn = DateTime.UtcNow;
+
+            await this.data.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<FeedbackViewModel> GetById(int id)
+            => await this.data.Feedbacks
+                .Where(x => x.Id == id && x.IsDeleted == false)
+                .ProjectTo<FeedbackViewModel>(this.mapper.ConfigurationProvider)
+                .FirstOrDefaultAsync();
+
+        public async Task SolveFeedback(int id)
+        {
+            var feedback = await this.data.Feedbacks
+                .FirstOrDefaultAsync(x => x.Id == id && x.IsDeleted == false);
+
+            feedback.IsSolved = true;
+            feedback.ModifiedOn = DateTime.UtcNow;
+
+            await this.data.SaveChangesAsync();
+        }
     }
 }
