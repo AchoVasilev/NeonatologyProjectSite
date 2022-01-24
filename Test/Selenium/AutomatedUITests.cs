d3ab0c46b8dd44a7dbb69dc456b4a733ad3101ae
AngelVasilev <defiend@abv.bg>
2022-01-24 23:16:25 +0200

    Add Selenium tests.

---
+++ Test/Selenium/AutomatedUITests.cs
@@ -0,0 +1,64 @@
+﻿namespace Test.Selenium
+{
+    using System;
+
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+
+    using Xunit;
+
+    public class AutomatedUITests : IDisposable
+    {
+        private readonly IWebDriver driver;
+        private const string Url = "https://localhost:5001/";
+
+        public AutomatedUITests()
+            => driver = new ChromeDriver();
+
+        public void Dispose()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+
+        [Fact]
+        public void HomeReturnsIndexView()
+        {
+            this.driver.Navigate()
+                .GoToUrl(Url);
+
+            Assert.Equal("Начало - Педиамед", this.driver.Title);
+            Assert.Contains("Добре дошли в Педиамед", this.driver.PageSource);
+        }
+
+        [Fact]
+        public void DoctorProfileReturnsView()
+        {
+            this.driver.Navigate()
+                .GoToUrl(Url + "Doctor/Profile");
+
+            Assert.Equal("Професионален профил - Педиамед", this.driver.Title);
+            Assert.Contains("Биография", this.driver.PageSource);
+        }
+
+        [Fact]
+        public void OfferAllReturnsView()
+        {
+            this.driver.Navigate()
+               .GoToUrl(Url + "Offer/All");
+
+            Assert.Equal("Нашите услуги - Педиамед", this.driver.Title);
+            Assert.Contains("Вид услуга", this.driver.PageSource);
+        }
+
+        [Fact]
+        public void GaleryAllReturnsView()
+        {
+            this.driver.Navigate()
+               .GoToUrl(Url + "Galery/All");
+
+            Assert.Equal("Галерия - Педиамед", this.driver.Title);
+            Assert.Contains("Галерия", this.driver.PageSource);
+        }
+    }
+}
