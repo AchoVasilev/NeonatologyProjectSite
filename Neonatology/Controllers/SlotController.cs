53555a461be58bcb2ce89605b045d0c8947d877e
AngelVasilev <defiend@abv.bg>
2022-05-02 20:15:29 +0300

    Edit slot controller

---
+++ Neonatology/Controllers/SlotController.cs
@@ -0,0 +1,29 @@
+﻿namespace Neonatology.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using Services.SlotService;
+
+    using static Common.GlobalConstants;
+
+    public class SlotController : BaseController
+    {
+        private readonly ISlotService slotService;
+
+        public SlotController(ISlotService slotService)
+        {
+            this.slotService = slotService;
+        }
+
+        [Authorize(Roles = DoctorConstants.DoctorRoleName)]
+        public async Task<IActionResult> TodaySlots()
+        {
+            var takenSlots = await this.slotService.GetTodaysTakenSlots();
+
+            return View(takenSlots);
+        }
+    }
+}
