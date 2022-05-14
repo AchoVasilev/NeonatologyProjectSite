b6fe5c28aa9b8008d779a153a8bd475b6c6baa1b
Angel <angavasilev@gmail.com>
2022-05-14 22:51:41 +0300

    SlotService Testing

--- Test/ServiceUnitTests/SlotServiceTests.cs
+++ Test/ServiceUnitTests/SlotServiceTests.cs
@@ -3,7 +3,9 @@
     using System;
     using System.Linq;
     using System.Threading.Tasks;
-
+    using Data.Models;
+    using Data.Models;
+    using Data.Models.Enums;
     using Services.SlotService;
 
     using Test.Mocks;
@@ -40,6 +42,137 @@
             var result = await service.GenerateSlots(startDate, endDate, 10, 5);
 
             Assert.Equal(6, dataMock.AppointmentSlots.Count());
+        }
+
+        [Fact]
+        public async Task GetGabrovoSlotsShouldReturnCorrectCount()
+        {
+            var dataMock = DatabaseMock.Instance;
+            var mapperMock = MapperMock.Instance;
+
+            var address = new Address()
+            {
+                Id = 5,
+                StreetName = "Kaspichan",
+                CityId = 1,
+                City = new City()
+                {
+                    Name = "Габрово"
+                }
+            };
+
+            await dataMock.Addresses.AddAsync(address);
+            await dataMock.SaveChangesAsync();
+
+            var service = new SlotService(dataMock, mapperMock);
+            var startDate = new DateTime(2022, 01, 25, 10, 0, 0);
+            var endDate = new DateTime(2022, 01, 25, 11, 0, 0);
+
+            var generatedSlots = await service.GenerateSlots(startDate, endDate, 30, 5);
+
+            var result = await service.GetGabrovoSlots();
+
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public async Task GetPlevenSlotsShouldReturnCorrectCount()
+        {
+            var dataMock = DatabaseMock.Instance;
+            var mapperMock = MapperMock.Instance;
+
+            var address = new Address()
+            {
+                Id = 5,
+                StreetName = "Kaspichan",
+                CityId = 1,
+                City = new City()
+                {
+                    Name = "Плевен"
+                }
+            };
+
+            await dataMock.Addresses.AddAsync(address);
+            await dataMock.SaveChangesAsync();
+
+            var service = new SlotService(dataMock, mapperMock);
+            var startDate = new DateTime(2022, 01, 25, 10, 0, 0);
+            var endDate = new DateTime(2022, 01, 25, 11, 0, 0);
+
+            var generatedSlots = await service.GenerateSlots(startDate, endDate, 30, 5);
+
+            var result = await service.GetPlevenSlots();
+
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public async Task GetFreeGabrovoSlotsShouldReturnCorrectCount()
+        {
+            var dataMock = DatabaseMock.Instance;
+            var mapperMock = MapperMock.Instance;
+
+            var address = new Address()
+            {
+                Id = 5,
+                StreetName = "Kaspichan",
+                CityId = 1,
+                City = new City()
+                {
+                    Name = "Габрово"
+                }
+            };
+
+            await dataMock.Addresses.AddAsync(address);
+            await dataMock.SaveChangesAsync();
+
+            var service = new SlotService(dataMock, mapperMock);
+            var startDate = DateTime.UtcNow;
+            var endDate = DateTime.UtcNow.AddHours(1);
+
+            var generatedSlots = await service.GenerateSlots(startDate, endDate, 20, 5);
+
+            var generatedSlot = dataMock.AppointmentSlots.First();
+            generatedSlot.Status = SlotStatus.Зает.ToString();
+            await dataMock.SaveChangesAsync();
+
+            var result = await service.GetFreeGabrovoSlots();
+
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public async Task GetFreePlevenSlotsShouldReturnCorrectCount()
+        {
+            var dataMock = DatabaseMock.Instance;
+            var mapperMock = MapperMock.Instance;
+
+            var address = new Address()
+            {
+                Id = 5,
+                StreetName = "Kaspichan",
+                CityId = 1,
+                City = new City()
+                {
+                    Name = "Плевен"
+                }
+            };
+
+            await dataMock.Addresses.AddAsync(address);
+            await dataMock.SaveChangesAsync();
+
+            var service = new SlotService(dataMock, mapperMock);
+            var startDate = new DateTime(2022, 01, 25, 10, 0, 0);
+            var endDate = new DateTime(2022, 01, 25, 11, 0, 0);
+
+            var generatedSlots = await service.GenerateSlots(startDate, endDate, 30, 5);
+            var generatedSlot = dataMock.AppointmentSlots.First();
+            generatedSlot.Status = SlotStatus.Зает.ToString();
+            await dataMock.SaveChangesAsync();
+
+            var result = await service.GetFreePlevenSlots();
+
+            Assert.Equal(1, result.Count);
         }
     }
 }
